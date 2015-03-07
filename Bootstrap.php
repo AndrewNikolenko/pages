0456ef09ca817eda25b8a3f1d5ad5e70969688bb
unknown <andruxanikolenko@yandex.ru>
2015-03-07 22:58:30 +0300

    Правка

--- Bootstrap.php
+++ Bootstrap.php
@@ -10,32 +10,6 @@ class Bootstrap implements BootstrapInterface
     /** @inheritdoc */
     public function bootstrap($app)
     {
-        $app->on(Application::EVENT_BEFORE_REQUEST, function () {
-             return [
-                'components' => [
-                    'urlManager' => [
-                        'enablePrettyUrl' => true,
-                        'showScriptName' => false,
-                        'rules' => [
-                            'page/<_c:(admin)>/index' => 'pages/admin/index',
-                            'page/<_a:(create)>' => 'pages/admin/<_a>',
-                            'page/<_a:(update)>/<id:[\w\-]+>' => 'pages/admin/update',
-                            'page/<_a:(delete)>/<id:[\w\-]+>' => 'pages/admin/delete',
-                            'page/<_a:(remove)>/<id:[\w\-]+>' => 'pages/admin/remove',
-                            'page/<_a:(restore)>/<id:[\w\-]+>' => 'pages/admin/restore',
-                            'page/<_a:(dropdown)>' => 'pages/admin/dropdown',
-                            'page/<_a:(dropdown)>' => 'pages/admin/dropdown',
-            
-                            'page/<_a:(a)>' => 'pages/admin/a',
-                            'page/<_a:(b)>' => 'pages/admin/b',
-                            'page/<_a:(c)>' => 'pages/admin/c',
-            
-                            'page/<_a:(contact)>' => 'pages/main/<_a>',
-                            'page/<id:[\w\-]+>' => 'pages/main/view',
-                        ],
-                    ],
-                ],
-             ];
-        });
+        
     }
 }
